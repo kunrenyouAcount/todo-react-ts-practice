216b03910b09e8026291b67c1c4bb4bd4f9bca21
kunrenyouAccount <1214kunrenyou@gmail.com>
2021-02-12 01:15:08 +0900

    ESLint、Prettierの導入と適用

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,8 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
+import React from 'react';
+import ReactDOM from 'react-dom';
 
-export const App = () => {
+export const App: React.FC = () => {
   return <h1>Hello React!!</h1>;
 };
 
-ReactDOM.render(<App />, document.getElementById("app"));
+ReactDOM.render(<App />, document.getElementById('app'));
